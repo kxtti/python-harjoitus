ad2ade0c110ae66e9a663cb7fdce2b8843b81d7d
Elias Lauanne <elias.lauanne@gmail.com>
2026-08-27 10:30:32 +0300

    koira

---
+++ module3/esimerkit2.py
@@ -0,0 +1,11 @@
+eka = -9
+toka = 12_456_123_180
+kolmas = 4.973
+neljäs = -4 + 2j
+
+print(eka)
+print(toka)
+print(kolmas)
+print(neljäs)
+print(neljäs.real)
+print(neljäs.imag)
---
+++ module3/tehtava1.py
@@ -0,0 +1,2 @@
+user = input ("Mikä on nimesi? ")
+print ('Terve ' + user +'!')
